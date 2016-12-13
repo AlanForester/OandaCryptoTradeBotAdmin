e04718b63961ed2b4e90549047ba326a88faef92
Alex Collin <alex@collin.su>
2016-12-13 07:56:42 +0300

    Init layout in component

--- client/main.ts
+++ client/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './imports/app/app.module';
 import '../both/methods/parties.methods';
 
 Meteor.startup(() => {
-  document.body.className = "web"
+  document.body.className = "pace-done fixed-nav fixed-nav-basic fixed-sidebar";
   const platform = platformBrowserDynamic();
   platform.bootstrapModule(AppModule);
 });
